234b8a0fc8f734bc682326c079f6c6a539f92d2c
rasyidcode7 <rasyidcode7@gmail.com>
2018-11-30 16:52:56 +0700

    completed half of the backend

--- routes/kuis.js
+++ routes/kuis.js
@@ -1,93 +1,155 @@
 const express = require('express')
 const router = express.Router()
+const Firestore = require('@google-cloud/firestore')
+const { Storage } = require('@google-cloud/storage')
+
+const Multer = require('multer')
+const multer = Multer({
+    storage: Multer.memoryStorage(),
+    limits: {
+        fileSize: 5 * 1024 * 1024
+    }
+})
+
+const keyFilename = 'firebase_sdk_key.json'
+const projectId = 'kuis-pubg-mobile-2019'
+const bucketName = `${projectId}.appspot.com`
+
+const firestore = new Firestore({
+    projectId: projectId,
+    keyFilename: keyFilename
+})
+const gcs = new Storage({
+    projectId,
+    keyFilename
+})
+
+const bucket = gcs.bucket(bucketName)
 
 router.get('/', (req, res, next) => {
-    let data = [
-        {
-            "id": "question1",
-            "question": "Apa nama game ini kalo anda tahu silahkan angkat kaki anda?",
-            "use_image": true,
-            "image": "../images/hero_background.jpg",
-            "correct": "pubgfffffffffffffffffffffffffffff",
-            "wrong": "dota 2",
-            "wrong2": "lol",
-            "wrong3": "mobalejen"
-        },
-        {
-            "id": "question1",
-            "question": "Apa nama game ini?",
-            "use_image": true,
-            "image": "../images/hero_background.jpg",
-            "correct": "pubg",
-            "wrong": "dota 2",
-            "wrong2": "lol",
-            "wrong3": "mobalejen"
-        },
-        {
-            "id": "question1",
-            "question": "Apa nama game ini?",
-            "use_image": true,
-            "image": "../images/hero_background.jpg",
-            "correct": "pubg",
-            "wrong": "dota 2",
-            "wrong2": "lol",
-            "wrong3": "mobalejen"
-        },
-        {
-            "id": "question1",
-            "question": "Apa nama game iniaa?",
-            "use_image": true,
-            "image": "../images/hero_background.jpg",
-            "correct": "pubg",
-            "wrong": "dota 2",
-            "wrong2": "lol",
-            "wrong3": "mobalejen"
-        },
-        {
-            "id": "question1",
-            "question": "Apa nama game iniaa?",
-            "use_image": true,
-            "image": "../images/hero_background.jpg",
-            "correct": "pubg",
-            "wrong": "dota 2",
-            "wrong2": "lol",
-            "wrong3": "mobalejen"
-        },
-        {
-            "id": "question1",
-            "question": "Apa nama game iniaa?",
-            "use_image": true,
-            "image": "../images/hero_background.jpg",
-            "correct": "pubg",
-            "wrong": "dota 2",
-            "wrong2": "lol",
-            "wrong3": "mobalejen"
-        }
-    ]
+    const document = firestore.collection('quiz')
+    document.get().then(dataquiz => {
+        let data = []
 
-    let convertedData = []
-    let convertedDataInside = []
-    data.forEach((question, index) => {
-        convertedDataInside.push(question)
+        dataquiz.forEach(doc => {
+            data.push(doc.data())
+        })
 
-        if (convertedDataInside.length == 3) {
-            convertedData.push(convertedDataInside)
-            convertedDataInside = []
-        }
+        console.log(data)
 
-        if (data.length % 3 != 0 && index == (data.length - 1) || (index-1) == (data.length - 2)) {
-            convertedData.push(convertedDataInside)
-        }
+        return data
+    }).then(data => {
+        let convertedData = []
+        let convertedDataInside = []
+        data.forEach((question, index) => {
+            convertedDataInside.push(question)
+
+            if (convertedDataInside.length == 3) {
+                convertedData.push(convertedDataInside)
+                convertedDataInside = []
+            }
+
+            if (data.length % 3 != 0 && index == (data.length - 1) || (index - 1) == (data.length - 2) && convertedDataInside.length != 0) {
+                convertedData.push(convertedDataInside)
+            }
+        })
+
+        console.log(convertedData.length)
+
+        res.render('home', { data: convertedData })
     })
-
-    console.log(convertedData.length)
-
-    res.render('home', { data: data})
 })
 
 router.get('/submit', (req, res, next) => {
-    res.render('submit')
+    let questionNumber
+    const document = firestore.collection('quiz')
+
+    // data.forEach((doc) => {
+        //     console.log(doc.data())
+        // })
+
+    document.get().then(data => {
+        questionNumber = data.size
+        questionNumber++
+
+        res.render('submit', { currentQuestion: questionNumber })
+    })
 })
+
+router.post('/submit', 
+    multer.single('image'),
+    sendUploadToGCS,
+    (req, res, next) => {
+
+    let data = req.body
+    const document = firestore.doc(`quiz/${data.quiz_id}`)
+
+    if (data.use_image === 'false') {
+        document.set({
+            id: data.quiz_id,
+            question: data.question,
+            use_image: data.use_image,
+            correct_answer: data.correct_answer,
+            wrong_answer1: data.wrong_answer1,
+            wrong_answer2: data.wrong_answer2,
+            wrong_answer3: data.wrong_answer3
+        }).then(() => {
+            res.redirect('/kuis/')
+        })   
+    } else if (data.use_image === 'true') {
+        if (req.file && req.file.cloudStoragePublicURL) {
+            document.set({
+                id: data.quiz_id,
+                question: data.question,
+                use_image: data.use_image,
+                image: req.file.cloudStoragePublicURL,
+                correct_answer: data.correct_answer,
+                wrong_answer1: data.wrong_answer1,
+                wrong_answer2: data.wrong_answer2,
+                wrong_answer3: data.wrong_answer3
+            }).then(() => {
+                res.redirect('/kuis/')
+            })
+        }
+    }
+})
+
+function sendUploadToGCS(req, res, next) {
+    if (!req.file) {
+        console.log('no image')
+        return next()
+    }
+
+    console.log('image exists')
+
+    const gcsname = Date.now() +'_'+ req.file.originalname;
+    const file = bucket.file(gcsname)
+    const stream = file.createWriteStream({
+        metadata: {
+            contentType: req.file.mimetype
+        },
+        resumable: false
+    })
+
+    stream.on('error', (err) => {
+        req.file.cloudStorageError = err
+        next(err)
+    })
+
+    stream.on('finish', () => {
+        req.file.cloudStorageObject = gcsname
+        file.makePublic().then(() => {
+            req.file.cloudStoragePublicURL = getPublicUrl(gcsname)
+            next()
+        })
+    })
+
+    stream.end(req.file.buffer)
+}
+
+function getPublicUrl(filename) {
+    return `https://storage.googleapis.com/${bucketName}/${filename}`
+}
 
 module.exports = router
 
